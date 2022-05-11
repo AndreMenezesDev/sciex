f6d5010c97db480a1865c0a50764d21ac709d9a3
Alan Spindler <alanspindler@live.com>
2022-05-11 16:03:10 -0400

    Remover hora da checagem do filtro

--- src/Suframa.Sciex.BusinessLogic/RelatorioListagemExportacaoAprovada/RelatorioListagemExportacaoBll.cs
+++ src/Suframa.Sciex.BusinessLogic/RelatorioListagemExportacaoAprovada/RelatorioListagemExportacaoBll.cs
@@ -2,6 +2,7 @@
 using Suframa.Sciex.DataAccess;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,8 +23,8 @@ namespace Suframa.Sciex.BusinessLogic
 			var retorno = new LEProdutoComplementoVM();
 			retorno.DataInicio = filtro.DataInicio != null ? new DateTime(filtro.DataInicio.Value.Year, filtro.DataInicio.Value.Month, filtro.DataInicio.Value.Day): filtro.DataInicio;
 			retorno.DataFim = filtro.DataFim != null ? new DateTime(filtro.DataFim.Value.Year, filtro.DataFim.Value.Month, filtro.DataFim.Value.Day) : filtro.DataFim;
-
-			if(filtro == null) { return null; }
+			
+			if (filtro == null) { return null; }
 			var listaLEProtudo = _uowSciex.QueryStackSciex.LEProduto.Listar(o => 
 					(
 					   filtro.InscricaoCadastral == 0 || o.InscricaoCadastral.ToString().StartsWith(filtro.InscricaoCadastral.ToString())
@@ -34,7 +35,7 @@ namespace Suframa.Sciex.BusinessLogic
 					)
 					&&
 					(
-						(filtro.DataInicio == null) || (o.DataAprovacao >= retorno.DataInicio && o.DataAprovacao <= retorno.DataFim)
+						(filtro.DataInicio == null) || (DbFunctions.TruncateTime(o.DataAprovacao) >= retorno.DataInicio && DbFunctions.TruncateTime(o.DataAprovacao) <= retorno.DataFim)
 					)
 					);
 			if (listaLEProtudo.Count == 0)
